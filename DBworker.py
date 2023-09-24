fdbb57921880d7607bb4bd2d90da24c9ba5c57b7
ProWega <deriswerderis@gmail.com>
2023-09-25 02:02:21 +0300

    ok

---
+++ DBworker.py
@@ -0,0 +1,75 @@
+import sqlite3
+import datetime
+import time
+import pandas as pd
+class DBparser:
+    def __init__(self):
+        self.oil_types_names = ['АИ-95']
+        connection = sqlite3.connect('azs.db')
+        cursor = connection.cursor()
+
+        cursor.execute('''SELECT * FROM type_oil''')
+        oil_types = cursor.fetchall()
+
+        self.oil_types_id = [el[0] for el in oil_types]
+        self.oil_types_names = [el[1] for el in oil_types]
+
+        self.oil_type_id_from_name = dict(zip(self.oil_types_names, self.oil_types_id))
+        self.name_oil_from_oil_type_id = dict(zip(self.oil_types_id, self.oil_types_names))
+
+        cursor.execute('''SELECT * FROM oil''')
+        oil = cursor.fetchall()
+
+        oil_ids = [el[0] for el in oil]
+        oil_names = [el[1] for el in oil]
+
+        self.oil_id_from_oil_name = dict(zip(oil_names, oil_ids))
+        self.oil_name_from_oil_id = dict(zip(oil_ids,oil_names))
+
+        cursor.execute('''SELECT * FROM station''')
+        station = cursor.fetchall()
+
+        id_stations = [el[0] for el in station]
+        links_station = [el[1] for el in station]
+
+        self.station_links_from_id = dict(zip(id_stations,links_station))
+        self.station_id_from_link = dict(zip(links_station, id_stations))
+
+        cursor.execute('''SELECT * FROM station_company''')
+        station_company = cursor.fetchall()
+        id_stations = [el[0] for el in station_company]
+        name_company = [el[1] for el in station_company]
+        self.station_companyname_from_id_station = dict(zip(id_stations, name_company))
+
+        connection.commit()
+        connection.close()
+    def GetSingleDayDf(self, date, name_oil_type):
+        id_oil_type = self.oil_type_id_from_name[name_oil_type]
+
+        connection = sqlite3.connect('azs.db')
+        cursor = connection.cursor()
+
+        cursor.execute(f''' SELECT id_oil FROM oil_id_type_oil_id WHERE type_oil_id={id_oil_type}''')
+        id_oil_to_df = [el[0] for el in cursor.fetchall()]
+        name_oil_to_df = [self.oil_name_from_oil_id[el] for el in id_oil_to_df]
+        print(f"Oil id to df: {id_oil_to_df}")
+
+
+        cursor.execute(f'''SELECT * FROM price WHERE date > {time.mktime(date.timetuple())} and date < {time.mktime(date.timetuple())+24*60*60}''')
+        price_table = cursor.fetchall()
+        station_links = [self.station_links_from_id[el[0]] for el in price_table]
+        oil_names = [self.oil_name_from_oil_id[el[1]] for el in price_table]
+        dates = [datetime.datetime.fromtimestamp(el[2]).strftime('%d-%m-%Y') for el in price_table]
+        name_station = [self.station_companyname_from_id_station[el[0]] for el in price_table]
+        price_values = [el[3] for el in price_table]
+
+        connection.commit()
+        connection.close()
+        df = pd.DataFrame(
+            data={"Название": name_station,
+                  "Ссылка": station_links,
+                  "Тип топлива": oil_names,
+                  "Цена": price_values}
+        )
+
+        return df[df['Тип топлива'].isin(name_oil_to_df)]
--- main.py
+++ main.py
@@ -1,24 +1,76 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
 import pandas as pd
+import sqlite3
+import DBworker
+import datetime
+from datetime import date
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-
+db = DBworker.DBparser()
+today = datetime.date.today()
+current_type_oil = db.oil_types_names[0]
+print(current_type_oil)
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = db.GetSingleDayDf(date(today.year, today.month, 24), current_type_oil)
+#df.to_csv('test_delete.csv')
+# data = GetDataPriceTableFromDB()
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Graph(id='graph-content'),
-dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection')
+    html.Div(children='Положение дел сегодня'),
+    html.Hr(),
+    dcc.RadioItems(options=db.oil_types_names, value=db.oil_types_names[0], id='controls-and-radio-item'),
+    #dcc.DatePickerSingle(
+    #    id='my-date-picker-single',
+    #    min_date_allowed=date(2023, 9, 24),
+    #    max_date_allowed=date(2027, 9, 19),
+    #    initial_visible_month=date(today.year, today.month, today.day),
+    #   date=date(today.year, today.month, today.day)
+    #),
+    html.Div(id='output-container-date-picker-single'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6, id='table'),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ])
 
+
+# Add controls to build the interaction
 @callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(col_chosen):
+    current_type_oil = col_chosen
+    print(current_type_oil)
+    df = db.GetSingleDayDf(date(2023, 9, 24), current_type_oil)
+    fig = px.histogram(df, x='Тип топлива', y="Цена", histfunc='avg')
+    return fig
+
+@callback(Output(component_id='table', component_property='data'),
+          Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_table(type_oil):
+    df = db.GetSingleDayDf(date(today.year, today.month, 24), type_oil)
+    return df.to_dict('records')
+
+
+
+
+@callback(
+    Output('output-container-date-picker-single', 'children'),
+    Input('my-date-picker-single', 'date'))
+def update_output(date_value):
+    string_prefix = 'Цены на: '
+    if date_value is not None:
+        date_object = date.fromisoformat(date_value)
+        date_string = date_object.strftime('%d-%m-%Y')
+        return string_prefix + date_string
+
+
+#@callback(Output(component_id='controls-and-graph', component_property='figure', allow_duplicate=True),
+#          Input(component_id='my-date-picker-single', component_property='date'))
+#def update_oil_prices(date):
+#    df = db.GetSingleDayDf(date, current_type_oil)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
